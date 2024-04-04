2640f968b626ba2a7cad65605d2321638a4db722
heyyrahul <rkkamat9213@gmail.com>
2024-04-05 03:52:38 +0530

    background image added

--- src/pages/AdminLogin.jsx
+++ src/pages/AdminLogin.jsx
@@ -41,6 +41,7 @@ const AdminLogin = () => {
 
   return (
     <div id='adminloginparent' >
+     
       <div className="admin-login-container" style={{backgroundColor:"#F7EEDD" ,boxShadow: "rgba(0, 0, 0, 0.25) 0px 54px 55px, rgba(0, 0, 0, 0.12) 0px -12px 30px, rgba(0, 0, 0, 0.12) 0px 4px 6px, rgba(0, 0, 0, 0.17) 0px 12px 13px, rgba(0, 0, 0, 0.09) 0px -3px 5px"}}>
         <h2><strong>Admin Login</strong></h2>
         <form onSubmit={handleSubmit} className="admin-login-form">
